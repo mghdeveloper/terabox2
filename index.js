e2a4bd8c6b55c9af1ad6aa64e6529a911b11cb54
mghdeveloper <140075630+mghdeveloper@users.noreply.github.com>
2025-11-02 02:34:20 +0100

    Update SELF_CHECK_URL to new server endpoint

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ await fs.promises.writeFile(COOKIES_FILE, JSON.stringify(cookies, null, 2));
     console.log(`Cookies updated and saved to ${COOKIES_FILE}`);
     await browser.close();
 }
-const SELF_CHECK_URL = "https://teraboxupload1-production.up.railway.app/hi";
+const SELF_CHECK_URL = "https://libby.onrender.com/hi";
 
 async function checkServerHealth() {
     try {
